ce7f231eca97d6336a86f541cc66badcdcc433d6
msmsolution <mangai.stephen@gmail.com>
2024-01-12 14:39:00 +0100

    Added add_string_numbers.py solution

---
+++ src/brain_teasers/python/add_string_numbers/src/add_string_numbers.py
@@ -0,0 +1,21 @@
+#Add string numbers
+#Given two strings of numbers, add the numbers together and return the result as a string.
+#Example:
+#Input: "123", "456"
+#Output: "579"
+#Input: "123", "456"
+#Output: "579"
+
+def add_string_numbers(num1, num2):
+    # Fill this in.
+    return str(int(num1) + int(num2))
+
+if __name__ == "__main__":  
+    print(add_string_numbers("123", "456"))
+    # 579
+    print(add_string_numbers("123", "456"))
+    # 579
+    print(add_string_numbers("123", "456"))
+
+
+
